e9738035ab1743f067512b1a100ec8361429e552
Ricardo3007 <ferrerricardo@hotmail.com>
2024-07-24 22:12:11 -0500

    actualizacion test

--- src/main/java/com/example/pruebabrasilia/controllers/TareaController.java
+++ src/main/java/com/example/pruebabrasilia/controllers/TareaController.java
@@ -13,28 +13,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/tarea")
-//@RequestMapping("api/{usuarioId}/tarea")
-//
+
 public class TareaController {
 
     @Autowired
     private TareaService _tareaService;
 
+    // Guardar Tarea relacionada a un usuario
     @PostMapping("/{usuarioId}")
     public ResponseEntity<Tarea> saveTarea(@PathVariable Integer usuarioId, @RequestBody Tarea tarea) {
         return new ResponseEntity<>(_tareaService.saveTarea(usuarioId, tarea), HttpStatus.OK);
     }
 
+    // Consultar tareas por usuarios
     @GetMapping("/{usuarioId}")
     public ResponseEntity<List<TareaUsuarioGetDTO>> getTareaUsuarioId(@PathVariable Integer usuarioId) {
         return new ResponseEntity<>(_tareaService.getTareaUsuarioId(usuarioId), HttpStatus.OK);
     }
 
+    // Actualizar Tarea
     @PutMapping("/{tareaId}")
     public ResponseEntity<Tarea> updateEstadoTarea(@PathVariable Integer tareaId) {
         return new ResponseEntity<>(_tareaService.updateEstadoTarea(tareaId), HttpStatus.OK);
     }
 
+    // Eliminar Tarea
     @DeleteMapping("/{tareaId}")
     public ResponseEntity<Boolean> deleteTarea(@PathVariable Integer tareaId) {
         return new ResponseEntity<>(_tareaService.deleteTarea(tareaId), HttpStatus.OK);
--- src/main/java/com/example/pruebabrasilia/controllers/UsuarioController.java
+++ src/main/java/com/example/pruebabrasilia/controllers/UsuarioController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -17,22 +16,26 @@ public class UsuarioController {
     @Autowired
     private UsuarioService _usuarioService;
 
+    // Consulta de usuarios
     @GetMapping
     public ResponseEntity<List<Usuario>> getUsuario() {
         return new ResponseEntity<>(_usuarioService.getUsuario(), HttpStatus.OK);
     }
 
+    // COnsulta Usuarrio por ID
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> getUsuarioId(@PathVariable Integer id) {
         return new ResponseEntity<>(_usuarioService.getUsuarioId(id), HttpStatus.OK);
     }
 
+    // Guardar Usuario
     @PostMapping
     public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario) {
         _usuarioService.saveUsuario(usuario);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
+    // Elmiminar Usuario
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> deleteUsuario(@PathVariable Integer id) {
         return new ResponseEntity<>(_usuarioService.deleteUsuario(id), HttpStatus.OK);
